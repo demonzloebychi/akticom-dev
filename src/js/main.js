6b7f995d80f9b30cbcfc0012212935e655653530
nikolai <adflame1342@gmail.com>
2024-05-15 23:56:43 +0300

    перенос неработающих скриптов в тег script (index, connect)

--- src/js/main.js
+++ src/js/main.js
@@ -133,35 +133,6 @@ for (i = 0; i < subLink.length; i++) {
   });
 }
 
-function openTab(event, id) {
-  var i, content, btn;
-
-  closestImage1 = document.querySelector(".main-tab-1");
-  closestImage2 = document.querySelector(".main-tab-2");
-  closestImage3 = document.querySelector(".main-tab-3");
-  defaultImage = document.querySelector(".defaul-image");
-  content = document.getElementsByClassName("item-tabs-base__img");
-  for (i = 0; i < content.length; i++) {
-    content[i].style.display = "none";
-    content[i].style.animation = "fadein 0.5s ease-in-out";
-  }
-
-  btn = document.getElementsByClassName("nav-tabs-base__item");
-  for (i = 0; i < btn.length; i++) {
-    btn[i].className = btn[i].className.replace(" is-active", "");
-  }
-
-  document.getElementById(id).style.display = "block";
-  event.currentTarget.className += " is-active";
-
-  if (event.type === "mouseout") {
-    closestImage1.style.display = "none";
-    closestImage2.style.display = "none";
-    closestImage3.style.display = "none";
-    defaultImage.style.display = "block";
-  }
-}
-
 let projectTab = function () {
   let projectTabNav = document.querySelectorAll(".nav-project__item"),
     projectTabContent = document.querySelectorAll(".project__body"),
@@ -193,63 +164,6 @@ let projectTab = function () {
   }
 };
 projectTab();
-
-var currentTab = 0;
-showTab(currentTab);
-
-function showTab(n) {
-  var x = document.getElementsByClassName("tab");
-  x[n].style.display = "block";
-  if (n == 0) {
-    document.getElementById("prevBtn").style.opacity = "0.5";
-    document.getElementById("prevBtn").disabled = true;
-  } else {
-    document.getElementById("prevBtn").style.opacity = "1";
-    document.getElementById("prevBtn").disabled = false;
-  }
-
-  fixStepIndicator(n);
-}
-
-function nextPrev(n) {
-  var x = document.getElementsByClassName("tab");
-  if (n == 1 && !validateForm()) return false;
-  x[currentTab].style.display = "none";
-  currentTab = currentTab + n;
-  if (currentTab >= x.length) {
-    document.getElementById("regForm").submit();
-    return false;
-  }
-  showTab(currentTab);
-}
-
-function validateForm() {
-  var x,
-    y,
-    i,
-    valid = true;
-  x = document.getElementsByClassName("tab");
-  y = x[currentTab].getElementsByTagName("input");
-  for (i = 0; i < y.length; i++) {
-    if (y[i].value == "") {
-      y[i].className += " invalid";
-      valid = false;
-    }
-  }
-  if (valid) {
-    document.getElementsByClassName("step")[currentTab].className += " finish";
-  }
-  return valid;
-}
-
-function fixStepIndicator(n) {
-  var i,
-    x = document.getElementsByClassName("step");
-  for (i = 0; i < x.length; i++) {
-    x[i].className = x[i].className.replace(" active", "");
-  }
-  x[n].className += " active";
-}
 
 const anchors = document.querySelectorAll("a[href*='#']");
 
