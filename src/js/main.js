21be3647b7ac4ca5f18c82cb21e239031c04105b
nikolas <nikolaislnkv@gmail.com>
2024-08-14 17:37:12 +0300

    blog and about

--- src/js/main.js
+++ src/js/main.js
@@ -32,36 +32,6 @@ function image_loaded() {
 }
 
 
-function openTab(event, id) {
-  var i, content, btn;
-
-  closestImage1 = document.querySelector(".main-tab-1");
-  closestImage2 = document.querySelector(".main-tab-2");
-  closestImage3 = document.querySelector(".main-tab-3");
-  defaultImage = document.querySelector(".defaul-image");
-  content = document.getElementsByClassName("item-tabs-base__img");
-  for (i = 0; i < content.length; i++) {
-    content[i].style.display = "none";
-    content[i].style.animation = "fadein 0.5s ease-in-out";
-  }
-
-  btn = document.getElementsByClassName("nav-tabs-base__item");
-  for (i = 0; i < btn.length; i++) {
-    btn[i].className = btn[i].className.replace(" is-active", "");
-  }
-
-  document.getElementById(id).style.display = "block";
-  event.currentTarget.className += " is-active";
-
-  if (event.type === "mouseout") {
-    closestImage1.style.display = "none";
-    closestImage2.style.display = "none";
-    closestImage3.style.display = "none";
-    defaultImage.style.display = "block";
-  }
-}
-
-
 const defImage = document.querySelector(".defaul-image");
 
 const animItems = document.querySelectorAll("._anim-items");
